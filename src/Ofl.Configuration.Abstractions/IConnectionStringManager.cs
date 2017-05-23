689ab7ee596488c71165a894c38a42353ee32c7e
casperOne <casperOne@caspershouse.com>
2017-05-23 16:53:15 -0400

    Initial code push.

---
+++ src/Ofl.Configuration.Abstractions/IConnectionStringManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ofl.Configuration.Abstractions
+{
+    public interface IConnectionStringManager
+    {
+        Task<string> GetConnectionStringAsync(string name, CancellationToken cancellationToken);
+    }
+}
